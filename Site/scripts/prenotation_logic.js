986edc992c9330e9f28b6939589d33eb68ec82ad
CiprianAgafitei <126824686+CiprianAgafitei@users.noreply.github.com>
2024-01-03 13:14:13 +0200

    Update prenotation_logic.js
    Inserite le impostazioni mancanti per la comunicazione con il database e piccoli miglioramenti.

--- Site/scripts/prenotation_logic.js
+++ Site/scripts/prenotation_logic.js
@@ -1,9 +1,6 @@
-// ===========================SELEZIONE DATA==================================
+//===========================SELEZIONE DATA==================================
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Ottieni l'elemento dell'input date
-    var dataInput = document.getElementById('dataSelezionata');
-
+document.addEventListener('DOMContentLoaded', function () {
     // Imposta la data minima consentita come la data attuale
     var dataAttuale = new Date();
     var annoAttuale = dataAttuale.getFullYear();
@@ -26,14 +23,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Imposta la data di default come la data attuale
     dataInput.setAttribute('value', dataMinima);
 
-    dataInput.addEventListener('keydown', function(event) {
+    dataInput.addEventListener('keydown', function (event) {
         event.preventDefault();
         return false;
     });
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Trova il paragrafo nel file incluso
     var paragrafoData = document.getElementById('data-description');
 
     // Aggiungi la classe 'appear' per attivare l'animazione
@@ -44,66 +40,60 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     var containerSection = document.getElementById('fieldContainerSection');
+
     var confirmationButtonContainer = document.getElementById('confirmationButtonContainer');
-    var confirmationButton = document.getElementById('confirmationButton');
-    var selectedButtons = [];
+    confirmationButtonContainer.style.display = 'none';
 
-    // Call the function to initially populate fields
-    updateFieldContainers();
+    var selectedButtons = [];        
+    let nrSelectedButtons = 0;
 
-    function getRandomName() {
-        var names = ['Field A', 'Field B', 'Field C', 'Field D', 'Field E'];
-        var randomIndex = Math.floor(Math.random() * names.length);
-        return names[randomIndex];
-    }
+    // Ottiene l'URL corrente
+    var urlParams = new URLSearchParams(window.location.search);
 
-    function updateFieldContainers() {
-        // Clear existing containers
+    // Recupera il valore del parametro 'option' dall'URL che conterrà lo sport scelto
+    var chosenSport = urlParams.get('option');
+
+    /** Aggiornamento del contenitore contenente gli orari disponibili */
+    function updateFieldContainers(data) {
         containerSection.innerHTML = '';
 
-        // Generazione container
-        for (var i = 0; i < 3; i++) {
-            var fieldContainer = document.createElement('div');
-            fieldContainer.className = 'field-container';
+        var fieldContainer = document.createElement('div');
+        fieldContainer.className = 'field-container';
 
-            var fieldName = document.createElement('h2');
-            fieldName.textContent = getRandomName();
+        var fieldName = document.createElement('h2');
+        fieldName.textContent = chosenSport;
 
-            var timeButtonsContainer = document.createElement('div');
-            timeButtonsContainer.className = 'time-buttons';
+        var timeButtonsContainer = document.createElement('div');
+        timeButtonsContainer.className = 'time-buttons';
 
-            // Esempio
-            var timeOptions = ['10:00 AM', '12:00 PM','10:00 AM', '12:00 PM','10:00 AM', '12:00 PM'];
-            var hasScrolled = false
+        var allTimeOptions = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00'];
 
-            timeOptions.forEach(function (option) {
-                var timeButton = document.createElement('input');
-                timeButton.type = 'button';
-                timeButton.value = option;
-                timeButton.addEventListener('click', function () {
+        var timeOptions = allTimeOptions.filter(element => !data.includes(element));
+
+        timeOptions.forEach(function (option) {
+            var timeButton = document.createElement('input');
+            timeButton.type = 'button';
+            timeButton.value = option;
+
+            timeButton.addEventListener('click', function () {
+                if (nrSelectedButtons < 2 || timeButton.classList.contains('selected')) {
                     timeButton.classList.toggle('selected');
                     updateSelectedButtons(timeButton);
                     updateConfirmationButtonVisibility();
-                    if (!hasScrolled) {
-                        scrollToConfirmationButton();
-                        hasScrolled = true;
-                    }
-                });
-                timeButtonsContainer.appendChild(timeButton);
-            });
 
-            fieldContainer.appendChild(fieldName);
-            fieldContainer.appendChild(timeButtonsContainer);
-            containerSection.appendChild(fieldContainer);
-        }
+                    nrSelectedButtons = document.querySelectorAll('.selected').length;
+                }
+            });
+            timeButtonsContainer.appendChild(timeButton);
+        });
+
+        fieldContainer.appendChild(fieldName);
+        fieldContainer.appendChild(timeButtonsContainer);
+        containerSection.appendChild(fieldContainer);
     }
 
-    confirmationButton.addEventListener('click', function () {
-        alert('Pagina conclusione prenotazione');
-    });
-
+    /** Aggiornamento dell'elenco dei pulsanti selezionati */
     function updateSelectedButtons(clickedButton) {
-        // Update the selected buttons array
         if (clickedButton.classList.contains('selected')) {
             selectedButtons.push(clickedButton);
         } else {
@@ -111,38 +101,55 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    /* Mostra o nascondi il pulsante per la conferma se, rispettivamente, è stato selezionato
+     *   almeno un'opzione oraria oppure nessuna */
     function updateConfirmationButtonVisibility() {
-        // Show or hide the confirmation button based on selection
         if (selectedButtons.length > 0) {
-            confirmationButtonContainer.style.opacity = '1';
+            confirmationButtonContainer.style.display = 'block';
         } else {
-            confirmationButtonContainer.style.opacity = '0';
+            confirmationButtonContainer.style.display = 'none';
         }
     }
 
     var dataSelezionata = document.getElementById('dataSelezionata');
 
+    /** Controllo sulla data selezionata in modo che sia selezionabile solo
+     *  quella successiva all'ora attuale del giorno attuale. */
     dataSelezionata.addEventListener('change', function () {
-        var selectedDate = new Date(dataSelezionata.value);
+        var chosenDate = new Date(dataSelezionata.value);
         var today = new Date();
         var oneMonthFromToday = new Date();
         oneMonthFromToday.setMonth(today.getMonth() + 1);
 
-        if (selectedDate < today) {
-            // Date is before today, display error popup
+        if (chosenDate < today) {
             displayErrorPopup("Si prega di selezionare una data successiva alla data attuale");
-            dataSelezionata.value = today; // Clear the invalid date
-            updateFieldContainers();
-        } else if (selectedDate > oneMonthFromToday) {
-            // Date is more than one month from today, display error popup
+            dataSelezionata.value = today;
+            containerSection.innerHTML = '';
+        } 
+        else if (chosenDate > oneMonthFromToday) {
             displayErrorPopup("Si prega a selezionare una data compresa tra la data corrente ed un mese dalla data corrente");
-            dataSelezionata.value = today; // Clear the invalid date
-            updateFieldContainers();
-        } else {
-            updateFieldContainers();
+            dataSelezionata.value = today;
+            containerSection.innerHTML = '';
+        }
+        else {
+            // Gestione data-selezionata-ricerca orari disponibili
+            var selectedDate = new Date(dataSelezionata.value).toISOString().split('T')[0];      
+            fetch('php/prenotation.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'selectedDate=' + selectedDate
+            })
+            .then(response => response.text())     
+            .then(data => {
+                updateFieldContainers(data);
+            })     
+            .catch(error => console.error('Errore:', error));
         }
     });
 
+    /** Effetti per la chisura della selezione della data */
     function closeDateSelectionDialog() {
         var datePicker = document.getElementById('dataSelezionata');
         if (datePicker) {
@@ -150,17 +157,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    /** Visualizzazione dei messaggi di errore relativi all'inserimento erratto della data */
     function displayErrorPopup(errorMessage) {
         closeDateSelectionDialog();
         var popupContainer = document.createElement('div');
         popupContainer.className = 'popup-container';
-    
+
         var popup = document.createElement('div');
         popup.className = 'popup';
-    
+
         var errorText = document.createElement('p');
         errorText.textContent = errorMessage;
-    
+
         var closeButton = document.createElement('input');
         closeButton.type = 'button';
         closeButton.value = 'Ho capito';
@@ -168,38 +176,47 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.removeChild(popupContainer);
             document.body.style.overflow = 'auto';
         });
-    
+
         popup.appendChild(errorText);
         popup.appendChild(closeButton);
         popupContainer.appendChild(popup);
-    
+
         document.body.appendChild(popupContainer);
         document.body.style.overflow = 'hidden';
     }
+ 
+    /* Aggiunta di un listener al form per intercettare la conferma della data e degli orari 
+    document.getElementById('set-prenotation').addEventListener('submit', function(event) {
+        var buttons = document.querySelectorAll('.dynamic-button');
+        var selectedValues = [];
 
+        // Loop sui bottoni per verificare quelli selezionati
+        buttons.forEach(function(button) {
+            if (button.classList.contains('selected')) {
+                selectedValues.push(button.value);
+            }
+        });
 
+        // Assegna i valori dei bottoni selezionati all'input nascosto
+        document.getElementById('selectedButtons').value = selectedValues.join(',');
+
+        return true;
+    });*/
 });
-
-function scrollToConfirmationButton() {
-    var confirmationButtonContainer = document.getElementById('confirmationButtonContainer');
-    var yOffset = confirmationButtonContainer.getBoundingClientRect().top + window.pageYOffset;
-    window.scrollTo({ top: yOffset, behavior: 'smooth' });
-}
 
 /** Visualizzazione del form per il login consentendo l'autenticazione senza la necessità
  * di reindirizzare l'utente sulla pagina Accedi, portando a numerosi vantaggi.
  */
 document.addEventListener('DOMContentLoaded', function () {
     const loginButton = document.getElementById('loginAutentication');
+    const overlay = document.getElementById('overlay');
+    const closeButton = document.getElementById('close');
+    const loginForm = document.getElementById('loginForm');
 
     // Autenticazione tramite login
     loginButton.addEventListener('click', () => {
         overlay.style.display = 'block';
     });
-
-    const overlay = document.getElementById('overlay');
-    const closeButton = document.getElementById('close');
-    const loginForm = document.getElementById('loginForm');
 
     // Nascondi la finestra modale quando si fa clic sul pulsante di chiusura
     closeButton.addEventListener('click', () => {
