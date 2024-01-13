7da9e29c85b1a21413d246ba63c1ca729b952c1d
CiprianAgafitei <126824686+CiprianAgafitei@users.noreply.github.com>
2024-01-13 16:17:21 +0100

    Update admin.php

--- Site/php/admin.php
+++ Site/php/admin.php
@@ -1,18 +1,63 @@
 <?php
     include_once "CSCAccess.php"; 
+    use CSC\CSCAccess; 
+    
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    setlocale(LC_ALL, 'it_IT');
 
-    use CSC\CSCAccess;
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    $paginaHTML = file_get_contents('../admin.html');
+
+    $prenotazioni = "";
+    $messaggi = "";
 
     $csc = new CSCAccess();
+    $conn = $csc->openConnection();
     
-    if($csc->openConnection()) {
+    if($conn) 
+    {
         $prenotations = $csc->getAllPrenotations();
+
+        foreach ($prenotations as $prenotazione) 
+        {
+            $sport = $csc->getNomeAttivita($prenotazione["id_Attivita"]);
+
+            $prenotazioni .= "<tr>
+                                <td>{$prenotazione["data"]}</td>
+                                <td>{$prenotazione["ora"]}</td>
+                                <td>\"$sport\"</td>
+                                <td>1</td>
+                                <td>{$prenotazione["utente"]}</td>
+                            </tr>";
+        }
+
         $messages = $csc->getAllRequests();
-        $csc->closeConnection();
-        
-        echo json_encode($prenotations);
-        echo json_encode($messages);
-    } else {
-        echo json_encode(array("error" => "Unable to connect to database."));
+
+        foreach ($messages as $messaggio) 
+        {
+            $nome_utente = $csc->getNomeUtente($messaggio["email"]);
+
+            if ($messaggio["titolo"] === NULL) {
+                $messaggio["titolo"] = "Senza titolo";
+            }
+
+            $messaggi .= "<div class=\"message-box\">
+                            <h3 class=\"message-title\">{$messaggio["titolo"]}</h3>
+                            <p class=\"message-name\">Nome: \"$nome_utente\"</p>
+                            <p class=\"message-email\">Email: {$messaggio["email"]}</p>
+                            <p class=\"message-content\">{$messaggio["testo"]}</p>
+                        </div>";
+        }
     }
+    $csc->closeConnection();
+
+    $paginaHTML = str_replace("{messaggi}", $messaggi, $paginaHTML);
+    $paginaHTML = str_replace("{righe_tabella}", $prenotazioni, $paginaHTML);
+
+    echo $paginaHTML;
 ?>
