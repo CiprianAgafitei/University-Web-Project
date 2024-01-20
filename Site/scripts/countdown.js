3cf4236569c4176460cba06163b3e9aeed3073e7
CiprianAgafitei <126824686+CiprianAgafitei@users.noreply.github.com>
2024-01-20 23:06:35 +0100

    Update countdown.js

--- Site/scripts/countdown.js
+++ Site/scripts/countdown.js
@@ -1,8 +1,8 @@
-
 window.onload = function() {
-    // Nasconde la seconda parte e mostra la prima
     document.getElementById('set-prenotation').style.display = "block";
     document.getElementById('end-prenotation').style.display = "none";
+    document.getElementById('show-pren-details').style.display = "none";
+    document.getElementById('logged-in-confirmation').style.display = "none";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -12,15 +12,50 @@ document.addEventListener('DOMContentLoaded', function() {
     const startButton = document.getElementById("continueButton");
     const circle = document.getElementById("circle");
 
-    startButton.addEventListener('click', function() {
-        // Avvia il conto alla rovescia di 10 minuti
-        countdownInterval = startCountdown(10, display); 
+    startButton.addEventListener('click', function () 
+    {   
+        // Distinzione per utente loggato e non
+        if (startButton.value === 'continua') 
+        {
+            // Blocco della risorsa nel database
+            var selectedDate = document.getElementById('dataSelezionata').value.toISOString().split('T')[0];      
+            fetch('../php/bloccaPrenotazione.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'selectedDate=' + selectedDate
+            })
+            .then(response => response.text())
+            .then(data => {
+                
+            })
+            .catch(error => console.error('Errore:', error));
 
-        // Nasconde la prima sezione e mostra la seconda
-        document.getElementById('set-prenotation').style.display = "none";
-        document.getElementById('end-prenotation').style.display = "block";
+            countdownInterval = startCountdown(10, display);
+            document.getElementById('set-prenotation').style.display = "none";
+            document.getElementById('end-prenotation').style.display = "block";
+        } 
+        // Altrimenti interviene php
+        else {
+            // Rilascio della risorsa dal database
+            
+            document.getElementById('set-prenotation').style.display = "none";
+            document.getElementById('show-pren-details').style.display = "block";
+            document.getElementById('logged-in-confirmation').style.display = "block";
+        }
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 
-        // Scorre in cima alla pagina
+    const backToTimes = document.getElementById("back-to-times");
+    backToTimes.addEventListener('click', function () 
+    {   
+        document.getElementById('set-prenotation').style.display = "block";
+        document.getElementById('show-pren-details').style.display = "none";
+        document.getElementById('logged-in-confirmation').style.display = "none";
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
@@ -28,31 +63,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     const backButton = document.getElementById("back-set-prenot");
-
-    backButton.addEventListener('click', function() {
-        // Ferma il contro alla rovescia
+    backButton.addEventListener('click', function () {
         stopCountdown();
-        // Aggiorna il valore di "display" su 10:00 in modo da evitare errori cliccando nuovamente conferma
         display.textContent = "10:00";
-        // Ritorna a mostrare gli elementi principali e a nascondere i secondari
+
         document.getElementById('set-prenotation').style.display = "block";
         document.getElementById('end-prenotation').style.display = "none";
     });
 
-    // Funzione per avviare il conto alla rovescia
     function startCountdown(duration, display) {
         let timer = duration * 60;
 
         const countdownInterval = setInterval(function() {
-            var minutes = Math.floor(timer / 60); // Calcolo dei minuti
-            var seconds = timer % 60; // Calcolo dei secondi
+            var minutes = Math.floor(timer / 60);
+            var seconds = timer % 60;
 
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 
             display.textContent = minutes + ":" + seconds;
 
-            // Calcola la percentuale rimanente del tempo
             percentage = (timer / (duration * 60)) * 100;
 
             circleTimer.style.background = `conic-gradient(rgb(226, 157, 29) ${percentage}%, transparent ${percentage}%)`;
@@ -63,11 +93,9 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, 1000);
 
-        // Ritorna l'intervallo per poterlo utilizzare esternamente
         return countdownInterval; 
     }
 
-    // Ferma il conto alla rovescia
     function stopCountdown() {
         clearInterval(countdownInterval);
     }
