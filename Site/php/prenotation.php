13c800acb9cb5b40848fe9bf6d92dfd714ab1506
CiprianAgafitei <126824686+CiprianAgafitei@users.noreply.github.com>
2024-01-20 23:19:08 +0100

    Update prenotation.php

--- Site/php/prenotation.php
+++ Site/php/prenotation.php
@@ -2,24 +2,62 @@
     require_once "CSCAccess.php";
     use CSC\CSCAccess;
 
-    session_start();
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    setlocale(LC_ALL, 'it_IT');
+
+    // Ottenimento del contenuto di accedi.php senza file_get_contents
+    $paginaHTML = file_get_contents("../prenotazioni.html");
+
+    function pulisciInput($value) {
+        $value = trim($value);
+        $value = strip_tags($value);
+        $value = htmlentities($value);
+        return $value;
+    }
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    $url = $_SERVER['REQUEST_URI'];
+    $url_components = parse_url($url);
+
+    // Verifica se ci sono parametri nella URL
+    if (isset($url_components['query'])) {
+        parse_str($url_components['query'], $params);
+
+        // Verifica se il parametro 'option' è presente nei parametri
+        if (isset($params['option'])) {
+            $_SESSION['attivita_scelta'] = $params['option'];
+        }
+    }
+
+    // Verfica se loggato
+    $logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+
+    if ($logged_in) {
+        $continuaOconferma = "conferma";
+    }
+    else
+        $continuaOconferma = "continua";
+
 
     $cscAccess = new CSCAccess();
     $conn = $cscAccess->openConnection();
 
-    if (isset($_POST['selectedDate'])) 
+    if ($conn) 
     {
-        if ($conn) 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
         {
-            $selectedDate = $_POST['selectedDate'];
-            $result = $cscAccess->getReservedPrenotations($selectedDate);
-            echo json_encode($result);  
+            // Ricarico il valore dell'attività scelta della sessione
+            $attivita_scelta = $_SESSION['attivita_scelta'];
         }
     }
-        
     $closeResult = $cscAccess->closeConnection();
-    
 
-    // Salvataggio data e orari selezionati con prenotazione temporanea su server per 10 minuti
+    $paginaHTML = str_replace("{Continua_O_Conferma}", $continuaOconferma, $paginaHTML);
 
+    echo $paginaHTML;
 ?>
