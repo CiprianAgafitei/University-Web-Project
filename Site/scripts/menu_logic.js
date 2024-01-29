23c40cac133aabf3e918480fab7b5dd23bf72581
CiprianAgafitei <126824686+CiprianAgafitei@users.noreply.github.com>
2024-01-29 17:32:35 +0100

    Update menu_logic.js

--- Site/scripts/menu_logic.js
+++ Site/scripts/menu_logic.js
@@ -6,7 +6,7 @@ function checkWindowSize() {
 
     if (screenWidth <= 1032) {
         // Schermo piccolo o uguale a 1032px (mobile)
-        menuLinks.forEach(function(link) {
+        menuLinks.forEach(function (link) {
             link.setAttribute('aria-hidden', 'false');
             link.setAttribute('tabindex', '0');
         });
@@ -14,7 +14,7 @@ function checkWindowSize() {
         desktopMenu.setAttribute('tabindex', '-1');
     } else {
         // Schermo più grande di 1032px (desktop)
-        menuLinks.forEach(function(link) {
+        menuLinks.forEach(function (link) {
             link.setAttribute('aria-hidden', 'true');
             link.setAttribute('tabindex', '-1');
         });
@@ -23,8 +23,8 @@ function checkWindowSize() {
     }
 }
 
-window.onload = function() { 
-    window.addEventListener('scroll', function() {
+window.onload = function () {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 100) {
             document.querySelector("header").classList.add('is-scrolling');
         }
@@ -36,10 +36,31 @@ window.onload = function() {
     const menu_button = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
 
-    menu_button.addEventListener('click', function() {
+    menu_button.addEventListener('click', function () {
         menu_button.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
     });
+
+    const currentPath = window.location.pathname.replace(/\/[^\/]+$/, '');
+    let pathToAuthPhp = "";
+    if (currentPath.includes('/php'))
+        pathToAuthPhp = window.location.origin + currentPath + '/auth.php';
+    else
+        pathToAuthPhp = window.location.origin + currentPath + '/php/auth.php';
+ 
+    // Verifica se l'utente è loggato e aggiorna il testo del menu
+    const accessButtonDesktop = document.getElementById('login-link-desk');
+    const accessButtonMobile = document.getElementById('login-link-mob');
+    if (accessButtonDesktop !== null && accessButtonMobile !== null) 
+    {
+        fetch(pathToAuthPhp)
+        .then(response => response.text())
+        .then(data => {
+            const parsedData = JSON.parse(data);
+            accessButtonDesktop.textContent = parsedData.logged_in ? 'Area riservata' : 'Accedi';
+            accessButtonMobile.textContent = parsedData.logged_in ? 'Area riservata' : 'Accedi';
+        });
+    }
 }
 
 window.addEventListener('DOMContentLoaded', checkWindowSize);
